6987c383bf1a1191b0ebba477cd5c1502da7855b
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 16:00:42 +0700

    add

---
+++ sugarloaf_savedata/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace sugarloaf_savedata {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name_header = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.step = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.detail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.min = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.max = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.name_header,
+            this.step,
+            this.detail,
+            this.min,
+            this.max});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1185, 391);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // number
+            // 
+            this.number.HeaderText = "number";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            this.number.Width = 50;
+            // 
+            // name_header
+            // 
+            this.name_header.HeaderText = "name header";
+            this.name_header.Name = "name_header";
+            this.name_header.ReadOnly = true;
+            this.name_header.Width = 180;
+            // 
+            // step
+            // 
+            this.step.HeaderText = "step";
+            this.step.Name = "step";
+            this.step.ReadOnly = true;
+            this.step.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.step.Width = 80;
+            // 
+            // detail
+            // 
+            this.detail.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.detail.DefaultCellStyle = dataGridViewCellStyle8;
+            this.detail.HeaderText = "detail";
+            this.detail.Name = "detail";
+            this.detail.ReadOnly = true;
+            this.detail.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // min
+            // 
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.min.DefaultCellStyle = dataGridViewCellStyle9;
+            this.min.HeaderText = "min";
+            this.min.Name = "min";
+            this.min.ReadOnly = true;
+            this.min.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.min.Width = 150;
+            // 
+            // max
+            // 
+            this.max.HeaderText = "max";
+            this.max.Name = "max";
+            this.max.ReadOnly = true;
+            this.max.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.max.Width = 150;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(272, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 436);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(1185, 171);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "number";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "name header";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 180;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "step";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn3.Width = 80;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTextBoxColumn4.DefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridViewTextBoxColumn4.HeaderText = "detail";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTextBoxColumn5.DefaultCellStyle = dataGridViewCellStyle12;
+            this.dataGridViewTextBoxColumn5.HeaderText = "min";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn5.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "max";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            this.dataGridViewTextBoxColumn6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn6.Width = 150;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            ".xlsx",
+            ".ods"});
+            this.comboBox2.Location = new System.Drawing.Point(1045, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = ".xlsx";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 619);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name_header;
+        private System.Windows.Forms.DataGridViewTextBoxColumn step;
+        private System.Windows.Forms.DataGridViewTextBoxColumn detail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn min;
+        private System.Windows.Forms.DataGridViewTextBoxColumn max;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ sugarloaf_savedata/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Xls;
+
+namespace sugarloaf_savedata {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            DirectoryInfo lish_excel = new DirectoryInfo(Application.ExecutablePath.Replace(".EXE", ".exe").Replace("sugarloaf_savedata.exe", ""));
+            FileInfo[] Files = lish_excel.GetFiles("*.xlsx");
+            foreach (FileInfo file in Files) {
+                if (file.Name.Contains("~")) continue;
+                comboBox1.Items.Add(file.Name.Replace(file.Extension, ""));
+            }
+            FileInfo[] Filess = lish_excel.GetFiles("*.ods");
+            foreach (FileInfo file in Filess) {
+                if (file.Name.Contains("~")) continue;
+                comboBox1.Items.Add(file.Name.Replace(file.Extension, ""));
+            }
+        }
+
+        Workbook workbook = new Workbook();
+        Worksheet worksheet;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            string[] s_head = File.ReadAllText(comboBox1.Text + ".txt").Replace("\r\n", "|").Split('|');
+            string[] s_step = File.ReadAllText(comboBox1.Text + "_step.txt").Replace("\r\n", "|").Split('|');
+            try {
+                workbook.LoadFromFile(comboBox1.Text + comboBox2.Text);
+            } catch {
+                MessageBox.Show("_ปิด excel ก่อน");
+                return;
+            }
+            worksheet = workbook.Worksheets["info"];
+            int page_excel = 7;
+            string sheet_test = "";
+            sheet_test = worksheet.GetText(page_excel, 2);
+            try { worksheet = workbook.Worksheets[sheet_test]; } catch { MessageBox.Show("_กรุณาปิด excel ก่อน"); return; }
+            worksheet = workbook.Worksheets[sheet_test];
+            List<string> l = new List<string>();
+            string string_number_in_excel;
+            string string_step_in_excel;
+            string string_spec_min_in_excel;
+            string string_spec_max_in_excel;
+            int row_in_excel = 1;
+            while (true) {
+                row_in_excel++;
+                if (worksheet.Range[row_in_excel, 1].Style.KnownColor.ToString() == "Color5" || worksheet.Range[row_in_excel, 1].Style.KnownColor.ToString() == "Color2") {
+                    continue;
+                }
+                string_number_in_excel = worksheet.GetText(row_in_excel, 1);
+                string_step_in_excel = worksheet.GetText(row_in_excel, 2);
+                string_spec_min_in_excel = worksheet.GetNumber(row_in_excel, 3).ToString();
+                if (string_spec_min_in_excel == "NaN") string_spec_min_in_excel = worksheet.GetFormulaNumberValue(row_in_excel, 3).ToString();
+                if (string_spec_min_in_excel == "NaN") string_spec_min_in_excel = worksheet.GetText(row_in_excel, 3);
+                if (string_spec_min_in_excel == null) string_spec_min_in_excel = worksheet.GetFormulaStringValue(row_in_excel, 3);
+                if (string_spec_min_in_excel != null) string_spec_min_in_excel = string_spec_min_in_excel.Trim();
+                string_spec_max_in_excel = worksheet.GetNumber(row_in_excel, 4).ToString();
+                if (string_spec_max_in_excel == "NaN") string_spec_max_in_excel = worksheet.GetFormulaNumberValue(row_in_excel, 4).ToString();
+                if (string_spec_max_in_excel == "NaN") string_spec_max_in_excel = worksheet.GetText(row_in_excel, 4);
+                if (string_spec_max_in_excel == null) string_spec_max_in_excel = worksheet.GetFormulaStringValue(row_in_excel, 4);
+                if (string_spec_max_in_excel != null) string_spec_max_in_excel = string_spec_max_in_excel.Trim();
+                if (string_number_in_excel == null) {
+                    string_number_in_excel = worksheet.GetNumber(row_in_excel, 1).ToString();
+                    if (string_number_in_excel == "NaN") {
+                        break;
+                    }
+                }
+                l.Add(string_number_in_excel + ";" + string_step_in_excel + ";" + string_spec_min_in_excel + ";" + string_spec_max_in_excel);
+            }
+            for (int i = 0; i < s_head.Count(); i++) {
+                dataGridView1.Rows.Add(1);
+                dataGridView1.Rows[i].Cells[0].Value = i + 1;
+                dataGridView1.Rows[i].Cells[1].Value = s_head[i];
+                dataGridView1.Rows[i].Cells[2].Value = s_step[i];
+                string[] step = s_step[i].Split(',');
+                if (step.Count() == 1) {
+                    bool hh = false;
+                    if (step[0] == "data_fail") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_fail"; }
+                    if (step[0] == "data_Final_Result") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_Final_Result"; }
+                    if (step[0] == "data_DATE_TIME") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_DATE_TIME"; }
+                    if (step[0] == "data_FG") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_FG"; }
+                    if (step[0] == "data_WO") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_WO"; }
+                    if (step[0] == "data_TESTER_ID") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_TESTER_ID"; }
+                    if (step[0] == "data_prism_number") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_prism_number"; }
+                    if (step[0] == "data_Operator") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_Operator"; }
+                    if (step[0] == "data_mode") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_mode"; }
+                    if (step[0] == "data_Test_Start_Time") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_Test_Start_Time"; }
+                    if (step[0] == "data_Test_Finish_Time") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_Test_Finish_Time"; }
+                    if (step[0] == "data_Test_Total_Time") { hh = true; dataGridView1.Rows[i].Cells[3].Value = "ok data_Test_Total_Time"; }
+                    if (hh) goto hh_;
+                    for (int kk = 0; kk < l.Count; kk++) {
+                        string[] mmm = l[kk].Split(';');
+                        if (mmm[0] != step[0]) continue;
+                        dataGridView1.Rows[i].Cells[3].Value = mmm[1];
+                        dataGridView1.Rows[i].Cells[4].Value = mmm[2];
+                        dataGridView1.Rows[i].Cells[5].Value = mmm[3];
+                        l.Remove(l[kk]);
+                        break;
+                    }
+                    hh_:;
+                } else {
+                    string data = "";
+                    string data_min = "";
+                    string data_max = "";
+                    foreach (string gh in step) {
+                        for (int kk = 0; kk < l.Count; kk++) {
+                            string[] mmm = l[kk].Split(';');
+                            if (mmm[0] != gh) continue;
+                            data += mmm[1] + "\n";
+                            data_min += mmm[2] + "\n";
+                            data_max += mmm[3] + "\n";
+                            l.Remove(l[kk]);
+                            break;
+                        }
+                    }
+                    data = data.Substring(0, data.Count() - 1);
+                    data_min = data_min.Substring(0, data_min.Count() - 1);
+                    data_max = data_max.Substring(0, data_max.Count() - 1);
+                    dataGridView1.Rows[i].Cells[3].Value = data;
+                    dataGridView1.Rows[i].Cells[4].Value = data_min;
+                    dataGridView1.Rows[i].Cells[5].Value = data_max;
+                }
+            }
+            for (int i = 0; i < l.Count; i++) {
+                string[] s = l[i].Split(';');
+                dataGridView2.Rows.Add(1);
+                dataGridView2.Rows[i].Cells[2].Value = s[0];
+                dataGridView2.Rows[i].Cells[3].Value = s[1];
+                dataGridView2.Rows[i].Cells[4].Value = s[2];
+                try { dataGridView2.Rows[i].Cells[5].Value = s[3]; } catch { }
+                dataGridView2.Rows[i].DefaultCellStyle.BackColor = System.Drawing.Color.Gold;
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) {
+            for (int i = 0; i < dataGridView1.RowCount; i++) {
+                dataGridView1.Rows[i].DefaultCellStyle.BackColor = System.Drawing.Color.White;
+            }
+            dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = System.Drawing.Color.CornflowerBlue;
+        }
+    }
+}
